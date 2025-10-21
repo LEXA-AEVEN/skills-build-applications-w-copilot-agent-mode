4761d35b2d913e85288dbf61b61980405b7db521
LEXA-AEVEN <lexa@aevengroup.com>
2025-10-21 12:13:38 +0000

    add this changes

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,32 @@
+from djongo import models
+
+class Team(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100, unique=True)
+
+class User(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+
+class Activity(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
+    duration = models.IntegerField()  # minutes
+    calories = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Workout(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    suggested_for = models.ManyToManyField(User, blank=True)
+
+class Leaderboard(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    total_calories = models.IntegerField(default=0)
+    total_duration = models.IntegerField(default=0)
+    updated_at = models.DateTimeField(auto_now=True)
